4a7c96b0be7284ad9d224b8ad338b2b9d92ace31
Daniel Caaz <DCaazy@gmail.com>
2016-02-23 21:41:03 -0800

    Exclamation removal because I don't know what us.

--- lib/piggybak_stripe/payment_decorator.rb
+++ lib/piggybak_stripe/payment_decorator.rb
@@ -5,20 +5,20 @@ module PiggybakStripe
     included do
       attr_accessor :stripe_token
       #attr_accessible :stripe_token
-    
+
       validates_presence_of :stripe_token, :on => :create
-    
+
       [:month, :year, :payment_method_id].each do |field|
-        _validators.reject!{ |key, _| key == field }
-    
-        _validate_callbacks.reject! do |callback|
+        _validators.reject{ |key, _| key == field }
+
+        _validate_callbacks.reject do |callback|
           callback.raw_filter.attributes == [field]
-        end  
+        end
       end
-      
+
       def process(order)
         return true if !self.new_record?
-        
+
         calculator = ::PiggybakStripe::PaymentCalculator::Stripe.new(self.payment_method)
         Stripe.api_key = calculator.secret_key
         begin
@@ -27,7 +27,7 @@ module PiggybakStripe
                       :card => self.stripe_token,
                       :currency => "usd"
                     })
-            
+
           self.attributes = { :transaction_id => charge.id,
                               :masked_number => charge.card.last4 }
           return true
